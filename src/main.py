3933c3a9c1216eb56cf23377d42fe47a8c05f0d6
TheoCasella <theocasella@gmail.com>
2026-04-12 18:39:15 -0300

    [TCM] Ajustes no main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from src.models.usuarios import TechLead
-from src.models.usuarios import Desenvolvedor
-from src.models.usuarios import ProductManager
+from src.models.usuarios.tech_lead import TechLead
+from src.models.usuarios.desenvolvedor import Desenvolvedor
+from src.models.usuarios.product_manager import ProductManager
 
 def selecionar_perfil():
-    nome = input("Qual o seu nome?")
-    cargo = input("\nQual seu cargo?")
+    nome = input("\nQual o seu nome? ")
+    cargo = input("\nQual seu cargo? ")
 
     if cargo == "TechLead":
         return TechLead(nome)
@@ -14,3 +14,14 @@ def selecionar_perfil():
         return ProductManager(nome)
     else:
         return "Erro, cargo inválido"
+
+if __name__ == "__main__":
+    usuario = selecionar_perfil()
+    if isinstance(usuario, str):
+        print(usuario)
+    else:
+        print("\n*** MVP Perfis de Usuários ***")
+        print(f"Usuário: {usuario.nome}")
+        print(f"Cargo: {usuario.cargo}")
+        print(f"Interesses: {', '.join(usuario.interesses_principais)}")
+        print(f"Ação da IA: {usuario.get_foco_ia()}")
